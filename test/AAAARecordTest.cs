45e1ed553024a85e2273919fa323b6a10aaa815e
Richard Schneider <makaretu@gmail.com>
2018-07-19 14:03:05 +1200

    test(AAAARecord): IPv6 address with scope id (iface)

--- test/AAAARecordTest.cs
+++ test/AAAARecordTest.cs
@@ -27,6 +27,22 @@ namespace Makaretu.Dns
         }
 
         [TestMethod]
+        public void Roundtrip_ScopeId()
+        {
+            var a = new AAAARecord
+            {
+                Name = "emanon.org",
+                Address = IPAddress.Parse("fe80::7573:b0a8:46b0:bfea%17")
+            };
+            var b = (AAAARecord)new ResourceRecord().Read(a.ToByteArray());
+            Assert.AreEqual(a.Name, b.Name);
+            Assert.AreEqual(a.Class, b.Class);
+            Assert.AreEqual(a.Type, b.Type);
+            Assert.AreEqual(a.TTL, b.TTL);
+            Assert.AreEqual(IPAddress.Parse("fe80::7573:b0a8:46b0:bfea"), b.Address);
+        }
+
+        [TestMethod]
         public void Roundtrip_Master()
         {
             var a = new AAAARecord
