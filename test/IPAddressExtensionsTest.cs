6ae88f87e7314ebbe2356dd1a2070776b46a45f1
Richard Schneider <makaretu@gmail.com>
2019-06-09 14:08:18 +1200

    feat(IPAddressExtensions): GetArpaName
    Get the "arpa" name for an IPv4 or IPv6 address.  Used for reverse
    DNS lookup.

---
+++ test/IPAddressExtensionsTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace Makaretu.Dns
+{
+
+    [TestClass]
+    public class IPAddressExtensionsTest
+    {
+        [TestMethod]
+        public void ArpaName()
+        {
+            Assert.AreEqual("4.4.8.8.in-addr.arpa", IPAddress.Parse("8.8.4.4").GetArpaName());
+            Assert.AreEqual("b.a.9.8.7.6.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa", IPAddress.Parse("2001:db8::567:89ab").GetArpaName());
+        }
+
+    }
+}
