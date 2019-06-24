acdebfba413c89609ffd09bb3ac1d7528624cc78
Richard Schneider <makaretu@gmail.com>
2019-06-24 13:27:56 +1200

    feat: add DomainName as a first class object

---
+++ src/DomainName.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makaretu.Dns
+{
+    /// <summary>
+    /// A domain name consists of one or more parts, <see cref="Labels"/>, that are 
+    /// conventionally delimited by dots, such as "example.org".
+    /// </summary>
+    /// <remarks>
+    ///   Equality is based on the number of and contents of <see cref="Labels"/>.
+    /// </remarks>
+    public class DomainName : IEquatable<DomainName>
+    {
+        const string dot = ".";
+        const char dotChar = '.';
+        const string escapedDot = @"\.";
+
+        /// <summary>
+        ///   A sequence of labels that make up the domain name.
+        /// </summary>
+        /// <value>
+        ///   A sequece of strings.
+        /// </value>
+        /// <remarks>
+        ///   The last label is the TLD (top level domain).
+        /// </remarks>
+        public List<string> Labels { get; set; } = new List<string>();
+
+        /// <summary>
+        ///   Creates a new instance of the <see cref="DomainName"/> class from
+        ///   the specified name.
+        /// </summary>
+        /// <param name="name">
+        ///   The dot separated labels; such as "example.org".
+        /// </param>
+        /// <remarks>
+        ///   The name can contain backslash to escape a character.
+        ///   See <see href="https://tools.ietf.org/html/rfc4343">RFC 4343</see> 
+        ///   for the character escaping rules.
+        /// </remarks>
+        public DomainName(string name)
+        {
+            Parse(name);
+        }
+
+        /// <summary>
+        ///   Creates a new instance of the <see cref="DomainName"/> class from
+        ///   the sequence of label.
+        /// </summary>
+        /// <param name="labels">
+        ///   The <see cref="Labels"/>.
+        /// </param>
+        /// <remarks>
+        ///   The labels are not parsed; character escaping is not performed.
+        /// </remarks>
+        public DomainName(params string[] labels)
+        {
+            Labels.AddRange(labels);
+        }
+
+        /// <summary>
+        ///   Returns the textual representation.
+        /// </summary>
+        /// <returns>
+        ///   The concatenation of the <see cref="Labels"/> separated by a dot.
+        /// </returns>
+        /// <remarks>
+        ///   If a label contains a dot, then it is escaped with a backslash.
+        /// </remarks>
+        public override string ToString()
+        {
+            return string.Join(dot, Labels.Select(label => label.Replace(dot, escapedDot)));
+        }
+
+        void Parse(string name)
+        {
+            Labels.Clear();
+            var label = new StringBuilder();
+            var n = name.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                var c = name[i];
+
+                // An escaped character is either \C or \DDD.
+                if (c == '\\')
+                {
+                    c = name[++i];
+                    if (!char.IsDigit(c))
+                    {
+                        label.Append(c);
+                    }
+                    else
+                    {
+                        var number = c - '0';
+                        number = (number * 10) + (name[++i] - '0');
+                        number = (number * 10) + (name[++i] - '0');
+                        label.Append((char)number);
+                    }
+                    continue;
+                }
+
+                // End of label?
+                if (c == dotChar)
+                {
+                    Labels.Add(label.ToString());
+                    label.Clear();
+                    continue;
+                }
+
+                // Just part of the label.
+                label.Append(c);
+            }
+            if (label.Length > 0)
+            {
+                Labels.Add(label.ToString());
+            }
+
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            var that = obj as DomainName;
+            return (that == null)
+               ? false
+               : this.Equals(that);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(DomainName that)
+        {
+            var n = this.Labels.Count;
+            if (n != that.Labels.Count)
+            {
+                return false;
+            }
+            for (var i = 0; i < n; ++i)
+            {
+                if (this.Labels[i] != that.Labels[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///   Value equality.
+        /// </summary>
+        public static bool operator ==(DomainName a, DomainName b)
+        {
+            if (object.ReferenceEquals(a, b)) return true;
+            if (a is null) return false;
+            if (b is null) return false;
+
+            return a.Equals(b);
+        }
+
+        /// <summary>
+        ///   Value inequality.
+        /// </summary>
+        public static bool operator !=(DomainName a, DomainName b)
+        {
+            return !(a == b);
+        }
+
+    }
+}
---
+++ test/DomainNameTest.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Makaretu.Dns
+{
+    [TestClass]
+    public class DomainNameTest
+    {
+        [TestMethod]
+        public void Standard()
+        {
+            var name = new DomainName("my.example.org");
+
+            Assert.AreEqual(3, name.Labels.Count);
+            Assert.AreEqual("my", name.Labels[0]);
+            Assert.AreEqual("example", name.Labels[1]);
+            Assert.AreEqual("org", name.Labels[2]);
+
+            Assert.AreEqual("my.example.org", name.ToString());
+        }
+
+        [TestMethod]
+        public void TopLevelDomain()
+        {
+            var name = new DomainName("org");
+
+            Assert.AreEqual(1, name.Labels.Count);
+            Assert.AreEqual("org", name.Labels[0]);
+
+            Assert.AreEqual("org", name.ToString());
+        }
+
+        [TestMethod]
+        public void Root()
+        {
+            var name = new DomainName("");
+
+            Assert.AreEqual(0, name.Labels.Count);
+
+            Assert.AreEqual("", name.ToString());
+        }
+
+        [TestMethod]
+        public void EscapedDotCharacter()
+        {
+            var name = new DomainName(@"my\.example.org");
+
+            Assert.AreEqual(2, name.Labels.Count);
+            Assert.AreEqual("my.example", name.Labels[0]);
+            Assert.AreEqual("org", name.Labels[1]);
+
+            Assert.AreEqual(@"my\.example.org", name.ToString());
+        }
+
+        [TestMethod]
+        public void EscapedDotDigits()
+        {
+            var name = new DomainName(@"my\046example.org");
+
+            Assert.AreEqual(2, name.Labels.Count);
+            Assert.AreEqual("my.example", name.Labels[0]);
+            Assert.AreEqual("org", name.Labels[1]);
+
+            Assert.AreEqual(@"my\.example.org", name.ToString());
+        }
+
+        [TestMethod]
+        public void FromLabels()
+        {
+            var name = new DomainName("my.example", "org");
+
+            Assert.AreEqual(2, name.Labels.Count);
+            Assert.AreEqual("my.example", name.Labels[0]);
+            Assert.AreEqual("org", name.Labels[1]);
+
+            Assert.AreEqual(@"my\.example.org", name.ToString());
+        }
+
+        [TestMethod]
+        public void Equality()
+        {
+            var a = new DomainName(@"my\.example.org");
+            var b = new DomainName("my.example", "org");
+            var c = new DomainName(@"my\046example.org");
+            var other1 = new DomainName("example.org");
+            var other2 = new DomainName("org");
+
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(a, c);
+            Assert.AreNotEqual(a, other1);
+            Assert.AreNotEqual(a, other2);
+        }
+
+        [TestMethod]
+        public void HashEquality()
+        {
+            var a = new DomainName(@"my\.example.org");
+            var b = new DomainName("my.example", "org");
+            var c = new DomainName(@"my\046example.org");
+            var other1 = new DomainName("example.org");
+            var other2 = new DomainName("org");
+
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+            Assert.AreEqual(a.GetHashCode(), c.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), other1.GetHashCode());
+            Assert.AreNotEqual(a.GetHashCode(), other2.GetHashCode());
+        }
+    }
+}
